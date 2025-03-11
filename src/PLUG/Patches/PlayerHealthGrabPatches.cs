a622e0b47f4c7742a843b9825f59f5518cab4a15
Assuka <202172718+as5uka@users.noreply.github.com>
2025-03-11 23:20:47 +0000

    fix: vibrate when healing other player (hurting yourself)

---
+++ src/PLUG/Patches/PlayerHealthGrabPatches.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+namespace PLUG.Patches;
+
+public static class PlayerHealthGrabPatches
+{
+    internal static void Initialize()
+    {
+        IL.PlayerHealthGrab.Update += PlayerHealthGrabOnUpdate;
+    }
+
+    private static void PlayerHealthGrabOnUpdate(ILContext ctx)
+    {
+        var c = new ILCursor(ctx);
+        
+        c.GotoNext(
+            MoveType.After,
+            x => x.MatchCallvirt(typeof(PlayerHealth).GetMethod("HurtOther"))
+        );
+        c.Emit(OpCodes.Call, typeof(PlayerHealthGrabPatches).GetMethods().FirstOrDefault(x => x.Name == "Vibrate"));
+    }
+    
+    public static void Vibrate()
+    {
+        if (!Plugin.DeviceManager.IsConnected()) return;
+        
+        Plugin.DeviceManager.VibrateConnectedDevicesWithDuration(0.5f, 1f);
+    }
+}
